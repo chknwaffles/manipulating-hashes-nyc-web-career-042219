31539fbad0cd20bfdde952cfe44720c3009bc41c
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:37:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,12 +8,5 @@ def second_challenge
 
   #code your solution here!
   food_list = []
-  groceries.each do |cats, food|
-    food.each do |name|
-      food_list << name
-    end
-  end
-
-  
-  food_list
+  groceries.values
 end
